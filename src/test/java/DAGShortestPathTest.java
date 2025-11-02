4071512bc6da03ad2ab4daee6caffd1465c02955
Bakytzhan Kassymgali <kereyb01@gmail.com>
2025-11-02 23:41:48 +0500

    test: add unit tests for SCC, Topo, DAG-SP

--- src/test/java/DAGShortestPathTest.java
+++ src/test/java/DAGShortestPathTest.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import graph.core.Graph;
+import graph.core.Metrics;
+import graph.dagsp.DAGShortestPath;
+import graph.topo.TopologicalSorter;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DAGShortestPathTest {
+    @Test
+    public void testSPandLP() {
+        Graph dag = new Graph(5, true);
+        dag.addEdge(0,1,2); dag.addEdge(0,2,1); dag.addEdge(2,3,2); dag.addEdge(1,3,2); dag.addEdge(3,4,3);
+        var topo = new TopologicalSorter(new Metrics()).topoSort(dag);
+        var sp = new DAGShortestPath(new Metrics()).shortestPaths(dag, 0, topo);
+        assertEquals(0.0, sp.dist[0]);
+        assertEquals(1.0, sp.dist[2]);
+        assertEquals(3.0, sp.dist[3]);
+        assertEquals(6.0, sp.dist[4]);
+        var lp = new DAGShortestPath(new Metrics()).longestPath(dag, topo);
+        double max = Double.NEGATIVE_INFINITY;
+        for (double d : lp.dist) if (d > max) max = d;
+        assertEquals(7.0, max, 1e-9);
+    }
 }
--- src/test/java/TopologicalSorterTest.java
+++ src/test/java/TopologicalSorterTest.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import graph.core.Graph;
+import graph.core.Metrics;
+import graph.topo.TopologicalSorter;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TopologicalSorterTest {
+    @Test
+    public void testSimpleDAG() {
+        Graph dag = new Graph(4, true);
+        dag.addEdge(0,1,1); dag.addEdge(1,2,1); dag.addEdge(0,3,1);
+        TopologicalSorter t = new TopologicalSorter(new Metrics());
+        var order = t.topoSort(dag);
+        assertTrue(order.indexOf(0) < order.indexOf(1));
+        assertTrue(order.indexOf(1) < order.indexOf(2));
+    }
 }
